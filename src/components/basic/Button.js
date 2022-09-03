98646d35dc77f0ea6d7da5e20fc9bd0e23a852ae
michaelyuhe <2312744987@qq.com>
2022-09-03 13:03:42 +0800

    lint

--- src/components/basic/Button.js
+++ src/components/basic/Button.js
@@ -1,25 +1,26 @@
-import PropTypes from 'prop-types'
-import './index.css'
+import PropTypes from "prop-types";
+import "./index.css";
 
 const Button = ({ color, text, onClick }) => {
-
-    return (
-        <button onClick={onClick}
-            className='btn'
-            style={{ backgroundColor: color }}>
-            {text}
-        </button>
-    )
-}
+  return (
+    <button
+      onClick={onClick}
+      className="btn"
+      style={{ backgroundColor: color }}
+    >
+      {text}
+    </button>
+  );
+};
 
 Button.defaultProps = {
-    color: 'steelblue',
-    text: 'Button'
-}
+  color: "steelblue",
+  text: "Button",
+};
 
 Button.prototypes = {
-    text: PropTypes.string,
-    color: PropTypes.string,
-    onClick: PropTypes.func.isRequired
-}
-export default Button
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+};
+export default Button;
